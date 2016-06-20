84056f827d9b9d8e6b5ef3d2ee235d36c1ed2c54
Shlomi <shlomib1991@gmail.com>
2016-06-20 12:34:05 +0300

    display solution fix

--- Maze3dMVP/src/view/Maze2dDisplay.java
+++ Maze3dMVP/src/view/Maze2dDisplay.java
@@ -1,20 +1,22 @@
 package view;
+
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import algorithms.mazeGenerator.Direction;
 import algorithms.mazeGenerator.Maze3d;
 import algorithms.mazeGenerator.Position;
+
 public class Maze2dDisplay extends MazeDisplay {
-	private Maze3d currentMaze;
-	int LoadedMaze2 = 0;
+	private Maze3d	currentMaze;
+	int				LoadedMaze2	= 0;
 	public Maze2dDisplay(Composite parent, int style) {
-		super(parent, style);
-		this.setBackground(new Color(null, 255, 255, 255));
+		super(parent,style);
+		this.setBackground(new Color(null,255,255,255));
 	}
 	@Override
 	protected void drawMaze(PaintEvent e) {
-		if (mazeData == null) {
+		if(mazeData == null){
 			return;
 		}
 		// if (LoadedMaze2 == 1) {
@@ -37,16 +39,16 @@ public class Maze2dDisplay extends MazeDisplay {
 		// }
 		// LoadedMaze2 = 0;
 		// }
-		if ((mazeData != null && currentMaze == null) || (LoadedMaze2 == 1)) {
-			currentMaze = new Maze3d(mazeData.length, mazeData[0].length, mazeData[0][0].length);
-			for (int i = 0; i < mazeData[0][0].length; i++) {
-				for (int j = 0; j < mazeData[0].length; j++) {
-					for (int j2 = 0; j2 < mazeData.length; j2++) {
-						if (mazeData[j][j2][i] == 0) {
-							currentMaze.setFree(j2, j, i);
+		if((mazeData != null && currentMaze == null) || (LoadedMaze2 == 1)){
+			currentMaze = new Maze3d(mazeData.length,mazeData[0].length,mazeData[0][0].length);
+			for(int i = 0; i < mazeData[0][0].length; i++){
+				for(int j = 0; j < mazeData[0].length; j++){
+					for(int j2 = 0; j2 < mazeData.length; j2++){
+						if(mazeData[j][j2][i] == 0){
+							currentMaze.setFree(j2,j,i);
 						}
-						if (mazeData[j][j2][i] == 1) {
-							currentMaze.setWall(j2, j, i);
+						if(mazeData[j][j2][i] == 1){
+							currentMaze.setWall(j2,j,i);
 							// }
 						}
 					}
@@ -56,53 +58,52 @@ public class Maze2dDisplay extends MazeDisplay {
 			}
 			LoadedMaze2 = 0;
 		}
-		e.gc.setBackground(new Color(null, 150, 150, 150));
+		e.gc.setBackground(new Color(null,150,150,150));
 		int width = getSize().x;
 		int height = getSize().y;
 		int cellWidth = width / mazeData.length;
 		int cellHeight = height / mazeData[0].length;
 		// [rows][columns][levels]
-		for (int i = 0; i < mazeData.length; i++) {
-			for (int j = 0; j < mazeData[i].length; j++) {
+		for(int i = 0; i < mazeData.length; i++){
+			for(int j = 0; j < mazeData[i].length; j++){
 				int x = j * cellWidth;
 				int y = i * cellHeight;
-				if (mazeData[i][j][0] != 0) {
-					e.gc.fillRectangle(x, y, cellWidth, cellHeight);
+				if(mazeData[i][j][0] != 0){
+					e.gc.fillRectangle(x,y,cellWidth,cellHeight);
 				}
 			}
 		}
-		character.draw(e, cellWidth, cellHeight);
+		character.draw(e,cellWidth,cellHeight);
 	}
-
 	@Override
 	public void goFloorDown() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX(), pos.getY(), pos.getZ() - 1);
+		moveCharacter(pos.getX(),pos.getY(),pos.getZ() - 1);
 	}
 	@Override
 	public void goFloorUp() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX(), pos.getY(), pos.getZ() + 1);
+		moveCharacter(pos.getX(),pos.getY(),pos.getZ() + 1);
 	}
 	@Override
 	public void goUp() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX(), pos.getY() - 1, pos.getZ());
+		moveCharacter(pos.getX(),pos.getY() - 1,pos.getZ());
 	}
 	@Override
 	public void goDown() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX(), pos.getY() + 1, pos.getZ());
+		moveCharacter(pos.getX(),pos.getY() + 1,pos.getZ());
 	}
 	@Override
 	public void goRight() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX() + 1, pos.getY(), pos.getZ());
+		moveCharacter(pos.getX() + 1,pos.getY(),pos.getZ());
 	}
 	@Override
 	public void goLeft() {
 		Position pos = character.getPos();
-		moveCharacter(pos.getX() - 1, pos.getY(), pos.getZ());
+		moveCharacter(pos.getX() - 1,pos.getY(),pos.getZ());
 	}
 	@Override
 	protected void debugStatus() {
@@ -115,7 +116,7 @@ public class Maze2dDisplay extends MazeDisplay {
 		System.out.println(mazeData[(pos.x)][((pos.y) + 1)][(pos.z)]);
 	}
 	private boolean moveCharacter(int x, int y, int z) {
-		Position pos = new Position(x, y, z);
+		Position pos = new Position(x,y,z);
 		// if (currentMaze == null) {
 		/// print mazedata
 		// for (int j2 = 0; z < mazeData[0][0].length; z++) {
@@ -128,14 +129,14 @@ public class Maze2dDisplay extends MazeDisplay {
 		// }
 		// System.out.println("\n");
 		// System.out.println(currentMaze);
-		if ((x >= 0 && x < currentMaze.getMaze().length) && (y >= 0 && y < mazeData[1].length) && (z >= 0 && z < mazeData[0][1].length)) {
-			if (((currentMaze).getMaze()[pos.x][pos.y][pos.z] == 0)) {
+		if((x >= 0 && x < currentMaze.getMaze().length) && (y >= 0 && y < mazeData[1].length) && (z >= 0 && z < mazeData[0][1].length)){
+			if(((currentMaze).getMaze()[pos.x][pos.y][pos.z] == 0)){
 				Position p = character.getPos();
 				p.setX(x);
 				p.setY(y);
 				p.setZ(z);
 				character.setPos(p);
-				getDisplay().syncExec(new Runnable() {
+				getDisplay().syncExec(new Runnable(){
 					@Override
 					public void run() {
 						redraw();
@@ -154,13 +155,13 @@ public class Maze2dDisplay extends MazeDisplay {
 	public void setcurrMaze(Maze3d m) {
 		currentMaze = m;
 		int[][][] mazeData = new int[m.getColumns()][m.getRows()][m.getLevels()];
-		for (int i = 0; i < currentMaze.getLevels(); i++) {
-			for (int j = 0; j < currentMaze.getColumns(); j++) {
-				for (int j2 = 0; j2 < currentMaze.getRows(); j2++) {
-					if (currentMaze.getMaze()[j][j2][i] == 0) {
+		for(int i = 0; i < currentMaze.getLevels(); i++){
+			for(int j = 0; j < currentMaze.getColumns(); j++){
+				for(int j2 = 0; j2 < currentMaze.getRows(); j2++){
+					if(currentMaze.getMaze()[j][j2][i] == 0){
 						mazeData[j][j2][i] = 0;
 					}
-					if (currentMaze.getMaze()[j][j2][i] == 1) {
+					if(currentMaze.getMaze()[j][j2][i] == 1){
 						mazeData[j][j2][i] = 1;
 						// }
 					}
--- Maze3dMVP/src/view/MazeWindow.java
+++ Maze3dMVP/src/view/MazeWindow.java
@@ -1,4 +1,5 @@
 package view;
+
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowData;
@@ -34,62 +35,63 @@ import algorithms.mazeGenerator.Maze3d;
 import algorithms.search.Solution;
 import io.MyCompressorOutputStream;
 import io.MyDecompressorInputStream;
+
 public class MazeWindow extends BasicWindow {
-	private HashMap<String, Maze3d> mazes = new HashMap<String, Maze3d>();
+	private HashMap<String, Maze3d>	mazes				= new HashMap<String, Maze3d>();
 	//
-	private MazeDisplay mazeDisplay;
-	public Maze2dDisplay maze2dDisplay;
-	GameCharacter character = new GameCharacter();
-	Maze3d maze;
-	Text name;
-	Text x;
-	Text y;
-	int LoadedMaze = 0;
-	Text z;
-	String outString = "";
-	String[] selectedItems;
-	int AreThereAnyMazes = 0;
-	Image image = new Image(display, "images/game.png");
-	Clip music;
-	Clip sound;
-	MyView view;
-	Solution s;
+	private MazeDisplay				mazeDisplay;
+	public Maze2dDisplay			maze2dDisplay;
+	GameCharacter					character			= new GameCharacter();
+	Maze3d							maze;
+	Text							name;
+	Text							x;
+	Text							y;
+	int								LoadedMaze			= 0;
+	Text							z;
+	String							outString			= "";
+	String[]						selectedItems;
+	int								AreThereAnyMazes	= 0;
+	Image							image				= new Image(display,"images/game.png");
+	Clip							music;
+	Clip							sound;
+	MyView							view;
+	Solution						s;
 	@Override
 	protected void initWidgets() {
-		shell.setSize(1000, 750);
-		GridLayout layout = new GridLayout(2, false);
+		shell.setSize(1000,750);
+		GridLayout layout = new GridLayout(2,false);
 		shell.setLayout(layout);
-		Composite toolbar = new Composite(shell, SWT.FILL);
+		Composite toolbar = new Composite(shell,SWT.FILL);
 		RowLayout rowLayout = new RowLayout(SWT.VERTICAL);
 		toolbar.setLayout(rowLayout);
-		toolbar.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
+		toolbar.setLayoutData(new GridData(SWT.FILL,SWT.FILL,false,false));
 		////// MAZE NAME + X + Y + Z///////
-		Composite composite_1 = new Composite(toolbar, SWT.NONE);
-		composite_1.setLayout(new GridLayout(6, false));
-		composite_1.setLayoutData(new RowData(150, 61));
-		name = new Text(composite_1, SWT.BORDER);
-		GridData gd_MazeName = new GridData(SWT.FILL, SWT.CENTER, true, false, 6, 1);
+		Composite composite_1 = new Composite(toolbar,SWT.NONE);
+		composite_1.setLayout(new GridLayout(6,false));
+		composite_1.setLayoutData(new RowData(150,61));
+		name = new Text(composite_1,SWT.BORDER);
+		GridData gd_MazeName = new GridData(SWT.FILL,SWT.CENTER,true,false,6,1);
 		gd_MazeName.widthHint = 50;
 		name.setLayoutData(gd_MazeName);
-		Label lblX = new Label(composite_1, SWT.NONE);
-		lblX.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		Label lblX = new Label(composite_1,SWT.NONE);
+		lblX.setLayoutData(new GridData(SWT.RIGHT,SWT.CENTER,false,false,1,1));
 		lblX.setText("x");
-		x = new Text(composite_1, SWT.BORDER);
-		GridData gd_columns = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		x = new Text(composite_1,SWT.BORDER);
+		GridData gd_columns = new GridData(SWT.FILL,SWT.CENTER,true,false,1,1);
 		gd_columns.widthHint = 50;
 		x.setLayoutData(gd_columns);
-		Label lblY = new Label(composite_1, SWT.NONE);
-		lblY.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		Label lblY = new Label(composite_1,SWT.NONE);
+		lblY.setLayoutData(new GridData(SWT.RIGHT,SWT.CENTER,false,false,1,1));
 		lblY.setText("y");
-		y = new Text(composite_1, SWT.BORDER);
-		GridData gd_rows = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		y = new Text(composite_1,SWT.BORDER);
+		GridData gd_rows = new GridData(SWT.FILL,SWT.CENTER,true,false,1,1);
 		gd_rows.widthHint = 50;
 		y.setLayoutData(gd_rows);
-		Label lblZ = new Label(composite_1, SWT.NONE);
-		lblZ.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		Label lblZ = new Label(composite_1,SWT.NONE);
+		lblZ.setLayoutData(new GridData(SWT.RIGHT,SWT.CENTER,false,false,1,1));
 		lblZ.setText("z");
-		z = new Text(composite_1, SWT.BORDER);
-		GridData gd_levels = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		z = new Text(composite_1,SWT.BORDER);
+		GridData gd_levels = new GridData(SWT.FILL,SWT.CENTER,true,false,1,1);
 		gd_levels.widthHint = 50;
 		z.setLayoutData(gd_levels);
 		////////////////////////////// FOR TEST
@@ -105,50 +107,50 @@ public class MazeWindow extends BasicWindow {
 		////////////////////////////// MakingGUI//////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////
-		Button btnGenerateMaze = new Button(toolbar, SWT.PUSH);
+		Button btnGenerateMaze = new Button(toolbar,SWT.PUSH);
 		btnGenerateMaze.setText("GenerateMaze");
-		Label lblDisplayMazes = new Label(toolbar, SWT.NONE);
+		Label lblDisplayMazes = new Label(toolbar,SWT.NONE);
 		lblDisplayMazes.setText("DisplayMaze");
-		List listDisplayMaze = new List(toolbar, SWT.VERTICAL | SWT.BORDER);
-		Button btnDisplayMaze = new Button(toolbar, SWT.PUSH);
+		List listDisplayMaze = new List(toolbar,SWT.VERTICAL | SWT.BORDER);
+		Button btnDisplayMaze = new Button(toolbar,SWT.PUSH);
 		btnDisplayMaze.setText("DisplayMaze");
-		mazeDisplay = new Maze2dDisplay(shell, SWT.BORDER);
-		mazeDisplay.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		mazeDisplay = new Maze2dDisplay(shell,SWT.BORDER);
+		mazeDisplay.setLayoutData(new GridData(SWT.FILL,SWT.FILL,true,true));
 		// Creating a bar menu
-		Menu bar = new Menu(shell, SWT.BAR);
+		Menu bar = new Menu(shell,SWT.BAR);
 		shell.setMenuBar(bar);
 		// Making a File button in bar
-		MenuItem fileItem = new MenuItem(bar, SWT.CASCADE);
+		MenuItem fileItem = new MenuItem(bar,SWT.CASCADE);
 		fileItem.setText("File");
 		// Making a drop down menu with save/load options
-		Menu submenu = new Menu(shell, SWT.DROP_DOWN);
+		Menu submenu = new Menu(shell,SWT.DROP_DOWN);
 		fileItem.setMenu(submenu);
-		MenuItem saveItem = new MenuItem(submenu, SWT.PUSH);
-		MenuItem loadItem = new MenuItem(submenu, SWT.PUSH);
+		MenuItem saveItem = new MenuItem(submenu,SWT.PUSH);
+		MenuItem loadItem = new MenuItem(submenu,SWT.PUSH);
 		saveItem.setText("Save File \tCtrl+S");
 		saveItem.setAccelerator(SWT.CTRL + 'S');
 		loadItem.setText("Load File \tCtrl+Z");
 		loadItem.setAccelerator(SWT.CTRL + 'Z');
 		// Making a Music button in bar
-		MenuItem musicItem = new MenuItem(bar, SWT.CASCADE);
+		MenuItem musicItem = new MenuItem(bar,SWT.CASCADE);
 		musicItem.setText("Music");
 		// Making a drop down menu with a checkbox to enable/disable music
-		Menu submenu2 = new Menu(shell, SWT.DROP_DOWN);
+		Menu submenu2 = new Menu(shell,SWT.DROP_DOWN);
 		musicItem.setMenu(submenu2);
-		MenuItem OnOff = new MenuItem(submenu2, SWT.CHECK);
+		MenuItem OnOff = new MenuItem(submenu2,SWT.CHECK);
 		OnOff.setText("On/Off \tCtrl+M");
 		OnOff.setSelection(true);
 		OnOff.setAccelerator(SWT.CTRL + 'M');
-		Button btnSolveDFS = new Button(toolbar, SWT.PUSH);
+		Button btnSolveDFS = new Button(toolbar,SWT.PUSH);
 		btnSolveDFS.setText("Solve by DFS");
-		Button btnDisplaySolve = new Button(toolbar, SWT.PUSH);
+		Button btnDisplaySolve = new Button(toolbar,SWT.PUSH);
 		btnDisplaySolve.setText("Display Solve");
-		playMusic(new File("music/sound.wav"));
+		// playMusic(new File("music/sound.wav"));
 		////////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////
 		///////////////////////////// Listeners////////////////////////////////////////
 		//////////////////////////////////////////////////////////////////////////////
-		listDisplayMaze.addSelectionListener(new SelectionListener() {
+		listDisplayMaze.addSelectionListener(new SelectionListener(){
 			public void widgetSelected(SelectionEvent event) {
 				selectedItems = listDisplayMaze.getSelection();
 				outString = selectedItems[0];
@@ -157,27 +159,27 @@ public class MazeWindow extends BasicWindow {
 			public void widgetDefaultSelected(SelectionEvent arg0) {
 			}
 		});
-		btnGenerateMaze.addListener(SWT.MouseDown, new Listener() {
+		btnGenerateMaze.addListener(SWT.MouseDown,new Listener(){
 			@Override
 			// Checks if the maze exist or not aswell.
 			public void handleEvent(Event arg0) {
-				if (!(x.getText().equals(y.getText()))) {
+				if(!(x.getText().equals(y.getText()))){
 					displayMessage("X and Y must be equal");
 					return;
 				}
 				int temp = 0;
-				if (AreThereAnyMazes == 0) {
+				if(AreThereAnyMazes == 0){
 					setChanged();
 					notifyObservers("generate_maze_3d" + " " + name.getText() + " " + x.getText() + " " + y.getText() + " " + z.getText());
 					listDisplayMaze.add(name.getText());
 					AreThereAnyMazes = 1;
 					return;
-				} else if (AreThereAnyMazes == 1) {
+				} else if(AreThereAnyMazes == 1){
 					temp = listDisplayMaze.getItemCount();
 					selectedItems = listDisplayMaze.getItems();
 					outString = selectedItems[0];
-					for (int i = 0; i < temp; i++) {
-						if ((selectedItems[i].equals(name.getText()))) {
+					for(int i = 0; i < temp; i++){
+						if((selectedItems[i].equals(name.getText()))){
 							displayMessage("Maze already exist");
 							return;
 						}
@@ -190,15 +192,15 @@ public class MazeWindow extends BasicWindow {
 				}
 			}
 		});
-		btnDisplayMaze.addListener(SWT.MouseDown, new Listener() {
+		btnDisplayMaze.addListener(SWT.MouseDown,new Listener(){
 			@Override
 			public void handleEvent(Event arg0) {
 				String[] getlists = listDisplayMaze.getSelection();
-				if ((getlists.length) == 0) {
+				if((getlists.length) == 0){
 					displayMessage("Please select a maze");
 					return;
 				}
-				if (LoadedMaze == 1) {
+				if(LoadedMaze == 1){
 					Maze3d maze = getMaze(outString);
 					displayMaze(maze); // returns name instead of name
 					LoadedMaze = 0;
@@ -208,56 +210,56 @@ public class MazeWindow extends BasicWindow {
 				notifyObservers("display" + " " + outString);
 			}
 		});
-		OnOff.addListener(SWT.Selection, new Listener() {
+		OnOff.addListener(SWT.Selection,new Listener(){
 			@Override
 			public void handleEvent(Event arg0) {
-				if (OnOff.getSelection()) {
+				if(OnOff.getSelection()){
 					music.start();
 				} else
 					music.stop();
 			}
 		});
-		saveItem.addListener(SWT.Selection, new Listener() {
+		saveItem.addListener(SWT.Selection,new Listener(){
 			@Override
 			public void handleEvent(Event arg0) {
 				OutputStream out;
-				FileDialog fd = new FileDialog(shell, SWT.SAVE);
+				FileDialog fd = new FileDialog(shell,SWT.SAVE);
 				fd.setText("Save");
 				fd.setFilterPath("C:/");
 				String[] filterExt = {"*.maz"};
 				fd.setFilterExtensions(filterExt);
 				String selected = fd.open();
 				// System.out.println(maze.toByteArray());
-				if (selected != null) {
-					try {
+				if(selected != null){
+					try{
 						Maze3d currmazed = mazeDisplay.getcurrMaze();
 						out = new MyCompressorOutputStream(new FileOutputStream(new File(selected)));
 						out.write(currmazed.toByteArray());
 						System.out.println(currmazed.toByteArray().length);
 						out.flush();
 						out.close();
-					} catch (FileNotFoundException e) {
+					} catch(FileNotFoundException e){
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} catch (IOException e) {
+					} catch(IOException e){
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
 			}
 		});
-		loadItem.addListener(SWT.Selection, new Listener() {
+		loadItem.addListener(SWT.Selection,new Listener(){
 			@Override
 			public void handleEvent(Event args) {
 				InputStream in;
-				FileDialog fd = new FileDialog(shell, SWT.OPEN);
+				FileDialog fd = new FileDialog(shell,SWT.OPEN);
 				fd.setText("Open");
 				fd.setFilterPath("C:/");
 				String[] filterExt = {"*.maz"};
 				fd.setFilterExtensions(filterExt);
 				String selected = fd.open();
-				if (selected != null) {
-					try {
+				if(selected != null){
+					try{
 						in = new MyDecompressorInputStream(new FileInputStream(selected));
 						File file = new File(selected);
 						byte b[] = new byte[(int) ((file.length()) - 9)];
@@ -267,14 +269,14 @@ public class MazeWindow extends BasicWindow {
 						// get maze name without the full path
 						int start = fd.getFilterPath().length();
 						int end = selected.length();
-						String loadedmazename = selected.substring(start + 1, end - 4);
-						mazes.put(loadedmazename, loaded);
+						String loadedmazename = selected.substring(start + 1,end - 4);
+						mazes.put(loadedmazename,loaded);
 						listDisplayMaze.add(loadedmazename);
 						in.close();
-					} catch (FileNotFoundException e) {
+					} catch(FileNotFoundException e){
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					} catch (IOException e) {
+					} catch(IOException e){
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
@@ -283,24 +285,23 @@ public class MazeWindow extends BasicWindow {
 			}
 		});
 		////////////////////////// try solve/////////////////////////
-		btnSolveDFS.addListener(SWT.MouseDown, new Listener() {
+		btnSolveDFS.addListener(SWT.MouseDown,new Listener(){
 			@Override
 			// Checks if the maze exist or not aswell.
 			public void handleEvent(Event arg0) {
 				setChanged();
 				notifyObservers("solve" + " " + name.getText() + " " + "DFS");
 				// System.out.println(s);
-				
 				int temp = 0;
-				if (AreThereAnyMazes == 1) {
+				if(AreThereAnyMazes == 1){
 					AreThereAnyMazes = 1;
 					return;
-				} else if (AreThereAnyMazes == 1) {
+				} else if(AreThereAnyMazes == 1){
 					temp = listDisplayMaze.getItemCount();
 					selectedItems = listDisplayMaze.getItems();
 					outString = selectedItems[0];
-					for (int i = 0; i < temp; i++) {
-						if ((selectedItems[i].equals(name.getText()))) {
+					for(int i = 0; i < temp; i++){
+						if((selectedItems[i].equals(name.getText()))){
 							displayMessage("Mazes solve is ready");
 							return;
 						}
@@ -313,6 +314,15 @@ public class MazeWindow extends BasicWindow {
 				}
 			}
 		});
+		btnDisplaySolve.addListener(SWT.Selection,new Listener(){
+			@Override
+			public void handleEvent(Event arg0) {
+				selectedItems = listDisplayMaze.getSelection();
+				outString = selectedItems[0];
+				setChanged();
+				notifyObservers("display_solution" + " " + outString);
+			}
+		});
 		/////////////////////////////////////////////////
 		////////////////////////////// EndofGUI///////////
 		/////////////////////////////////////////////////
@@ -320,7 +330,7 @@ public class MazeWindow extends BasicWindow {
 	}
 	@Override
 	public void displayMessage(String message) {
-		display.syncExec(new Runnable() {
+		display.syncExec(new Runnable(){
 			@Override
 			public void run() {
 				MessageBox msg = new MessageBox(shell);
@@ -348,17 +358,19 @@ public class MazeWindow extends BasicWindow {
 	}
 	@Override
 	public void displaySolution(Solution s) {
-		// TODO Auto-generated method stub
+		System.out.println(s);
+//		for(Solution s : w){
+//		}
 	}
 	private void playMusic(File file) {
-		try {
+		try{
 			music = AudioSystem.getClip();
 			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(file)));
 			music.open(inputStream);
 			// loop infinitely
-			music.setLoopPoints(0, -1);
+			music.setLoopPoints(0,-1);
 			music.loop(Clip.LOOP_CONTINUOUSLY);
-		} catch (Exception e) {
+		} catch(Exception e){
 			e.printStackTrace();
 		}
 	}
